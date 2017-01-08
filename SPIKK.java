b6e73e77f0398817cbc4d5f7b763378a243987a6
kristdevelop <kyllirist@gmail.com>
2017-01-08 23:42:12 +0200

    notes collection

---
+++ SPIKK.java
@@ -0,0 +1,84 @@
+
+MÄRKMED
+
+CLASS > MAIN > METHOD having args (argumendid kogum väärtusi)
+// private tähendab, et see meetod on kättesaadav ainult siit failist.
+// static tähendab, et meetodi välja kutsumiseks ei pea eraldi Objekti looma.
+// boolean tähendab, et meetod peab lõpuks return'ima booleani tulemuseks
+
+public class KirjedMain {									//kutsume välja klassi meetodiga
+	public static void main(String[] args) { ..... }
+}
+public static int suvaT2ring() {							//?? public static int meetod arvu-genereerimiseks?
+        return (int) (Math.random() * 6 + 1);
+    }
+
+
+
+// ARGS LIST: 
+int	float double long		Math.abs() 
+		String nimetus = "miskikirjutis";
+		String taring[][] = {								//näitest: vurflite_liitmine
+		int nimetus = 150;	
+		int [] nimetus = {1, 222, 3, 77};  					//massiiv, NB arrays-klass importida 
+		int[][] lauaAlgseis = new int[9][9];
+		int m2ngijat2ring2 = 1 + (int) (Math.random() * 6);
+		lauaAlgseis[i][j] = (int) (Math.random() * 1.05); 	//???mis int peab siin olema (sulgudes int)
+		sisend = TextIO.getlnDouble();						//???mis see teeb
+	
+// suhtlemine
+	System.out.println("Sisesta: ");
+	System.out.printf("Nimi on: %s, tema vanus: %d ja palgaks saab %f%n", saadudNimi, saadudVanus, saadudPalk);  //%n kirjutab selle uuele reale
+	Scanner	klaviatuurilt = new Scanner(System.in);			//scanner enne import
+	//klaviatuur.next();									//tahetakse miskit sõne vastuseks, aga see ei salvetsu
+	String nimi = klaviatuurilt.next();						//nüüd aga salvestub, salvestab küsitu sisuliselt muutuja defineerimise kaudu. 
+	int arvnimi = klaviatuurilt.nextInt();					//Tegu enne oli sõne muutujaga, nüüd arvudega
+
+//tsükkel, tingimused:
+	if (antudArv > 90) { siis vasta sout println(); }
+	else if () { }
+	else { sout }
+//equals
+	if (sõne.equals("Tartu")) { .... }
+	if (a == b) { sout } 									//== nii ei soovita ei sõne ega arrays puhul
+
+	if (Arrays.equals() )
+
+	// = omistame miski väärtuse
+	// == see on nagu küsimärk, kas a on võrdne b? Ei: == "Tartu", vaid sõne.equals
+ 	if (a == 5 && b == 6) // mõlemad tõesed
+ 	if (a == 5 || b == 6) // üks neist tõene
+
+// Nullist üheksani:
+for (int i = 0; i < 9; i++) { 
+ j++	//lisab ühevõrra
+
+//while tsükkel
+ 	while() { sout } 										//niikaua kui tingimus täidetud. Töötab nii kaua kuni näeb break; käsku.
+ 	// Mängi enda tsükkel. 
+	while (true) {
+
+
+
+
+//NÄITEID:
+//Kuva 10 korda Tere!
+  int algus = 0;
+        while (algus < 10) {
+            System.out.println("Tere!");
+            algus = algus + 1;
+
+
+String[] xy = input.split("-"); // poolita x-y string kaheks
+
+int x = Integer.parseInt(xy[0]) - 1; // muuda string int'iks
+
+else if (tabamus == 0) {
+
+boolean labi = kasOnMangLabi(lauaAlgseis); // Käivita meetod, mis vaatab laua üle 
+	if (labi == true) { // Kui laevasid ei ole, siis
+		break; // läheb tsükkel katki ja...
+
+
+KÕIK
+}
