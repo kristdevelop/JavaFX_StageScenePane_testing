6b2738f3c451c400f7f8b855449a9dcade5bbdea
kristdevelop <kyllirist@gmail.com>
2017-01-14 17:56:13 +0200

    Update SPIKK2.java

--- SPIKK2.java
+++ SPIKK2.java
@@ -2,3 +2,12 @@ MUU
 
  if (!laevadTabamata()) {
                 //gameover kui pole alles, hüüumärk muudab väite vastupidiseks
+                
+ //seadistamsest         
+        manguvaljak = new GridPane();
+        HaldurMina = new Lauahaldus(manguvaljak, lauaPikkusLaevades, lauaLaiuslaevades, laevaPikkusPx);    //see annabki lauale halduse õigused. Tekkis aga ei tee midagi
+        Scene scene = new Scene(manguvaljak, lauaPikkusLaevades * laevaPikkusPx, lauaLaiuslaevades * laevaPikkusPx);
+        mainGameSquare = new Stage();
+        scene.setFill(Color.DEEPSKYBLUE);
+        mainGameSquare.setScene(scene);
+        mainGameSquare.show();
