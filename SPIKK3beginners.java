0fe15749d755c31ce935501727091571f751dad6
kristdevelop <kyllirist@gmail.com>
2017-01-15 22:55:28 +0200

    Update SPIKK3beginners.java

--- SPIKK3beginners.java
+++ SPIKK3beginners.java
@@ -1,32 +1,17 @@
 GIT https://wiki.itcollege.ee/index.php/I253_Informatika_eelkursus#Neljas_teema:_Git_ja_SSH_v.C3.B5tmed
-
 CODES https://github.com/caveofprogramming/java-beginners.git
 
 KOODE
 public class Hobune {       //ütle 
-    int kaal = 500;
-    String nimi = "Hopsu";
-
     public static void main(String[] args) {
         System.out.println("H-H-H-Hopssuuuu..");
-    }
-}
+    }}
 
 
-public class Program {
-    public static void main(String[] args) {
-
-        // Test true and false booleans.
-        boolean value = true;
-        if (value) {
-            System.out.println("A");
-        }
+public class Program {public static void main(String[] args) { boolean value = true; // seadistame, et õige
+        if (value) {System.out.println("prindin konsooli õige");}
         value = false;
-        if (!value) {
-            System.out.println("B");
-        }
-    }
-}
-
+       if (!value) {System.out.println("prindin konsooli vale");}
+    }}
 
 
