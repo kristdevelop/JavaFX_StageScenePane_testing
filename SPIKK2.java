cbb7ed400b5451cd49e8538d2f0cd9f729d53104
kristdevelop <kyllirist@gmail.com>
2017-01-16 01:17:51 +0200

    Update SPIKK2.java

--- SPIKK2.java
+++ SPIKK2.java
@@ -67,26 +67,6 @@ IF SWITCH TRY-CATCH
 
 
 
-Java FX
-Stage - aken
-
-
-//TULEMUSEKS AKEN RINGIGA
-import javafx.application.Application;
-import javafx.scene.Scene;
-import javafx.scene.layout.StackPane;
-import javafx.scene.shape.Circle;
-import javafx.stage.Stage;
-public class Main extends Application{
-    @Override
-    public void start(Stage primaryStage) throws Exception {
-        StackPane stack = new StackPane();
-        Scene scene = new Scene(stack, 150, 150);
-        primaryStage.setScene(scene);
-        Circle ring = new Circle(30);
-        stack.getChildren().add(ring);
-        primaryStage.show();}               }
-
 
 
 
