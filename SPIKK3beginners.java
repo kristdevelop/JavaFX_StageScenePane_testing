bd50154a8a2b1be01c00c6f49a248f017b844331
kristdevelop <kyllirist@gmail.com>
2017-01-15 21:14:28 +0200

    Update SPIKK3beginners.java

--- SPIKK3beginners.java
+++ SPIKK3beginners.java
@@ -1,1 +1,3 @@
-https://github.com/caveofprogramming/java-beginners.git
+GIT https://wiki.itcollege.ee/index.php/I253_Informatika_eelkursus#Neljas_teema:_Git_ja_SSH_v.C3.B5tmed
+
+CODES https://github.com/caveofprogramming/java-beginners.git
