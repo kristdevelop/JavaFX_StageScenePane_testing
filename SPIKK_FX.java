aa9d0e5a4208f37b7581c0c44296c38514b97b62
kristdevelop <kyllirist@gmail.com>
2017-01-16 04:00:59 +0200

    Update SPIKK_FX.java

--- SPIKK_FX.java
+++ SPIKK_FX.java
@@ -18,3 +18,24 @@ public class MinuLoodudKlass extends Application{                          //lai
         Circle joonistanRingi = new Circle(30);
         kujundMinu.getChildren().add(joonistanRingi);                          //vajalik!!!
     }}
+
+
+
+1. Pane = StackPane on parem
+ Pane kujund = new Pane();
+ Scene stseen = new Scene(kujund, 500, 100);
+
+2. BorderPane = veebiülesehitus
+3. StackPane = kui on vaja elemente üksteise peale sobitada NING elemendid satuvad vaikimisi keskele
+4. GridPane = ruudustik
+5. VBox = paigutab elemendid üksteise alla
+6. Hbox = paigutab elemendid vasakult paremale
+7. FlowPane = paigutab elemendid vasakult paremale, kuid kastide vahel on õhuruumi
+8. TilePane = nagu eelmine, aga kastid ühesuurused. //tile.setPrefColumns(2); Määra eelistatud tulpade arvuks 3
+9. AnchorPane = elemendid saab asetada nurkadesse
+
+Kasutajaliides
+10. Label ehk silt on lihtne tekst
+
+
+
