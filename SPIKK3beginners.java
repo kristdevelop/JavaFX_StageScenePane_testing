a1669408d2dcab385ff6b5c51185af0919a8df90
kristdevelop <kyllirist@gmail.com>
2017-01-16 13:12:37 +0200

    Update SPIKK3beginners.java

--- SPIKK3beginners.java
+++ SPIKK3beginners.java
@@ -1,5 +1,6 @@
 GIT https://wiki.itcollege.ee/index.php/I253_Informatika_eelkursus#Neljas_teema:_Git_ja_SSH_v.C3.B5tmed
 CODES https://github.com/caveofprogramming/java-beginners.git
+REVIEW JAVAFX http://docs.oracle.com/javafx/2/get_started/form.htm
 
 KLASSID
 1.1. public class Main extends Application {
