4be391287630f5165b556eac87220358cf9f867a
kristdevelop <kyllirist@gmail.com>
2017-01-16 03:05:47 +0200

    Update SPIKK_FX.java

--- SPIKK_FX.java
+++ SPIKK_FX.java
@@ -1,18 +1,22 @@
-Stage - aken
-
+Stage on esimene aken
+Scene on tegevusstseen
+Layout ehk StackPane? //kuidas asjad paigutavad, Stseeni ilma layoutita teha ei saa, seep StackPane Scene() sulgude vahel
+Node ehk esemed
 
 //kood, TULEMUSEKS AKEN RINGIGA
-import javafx.application.Application;
-import javafx.scene.Scene;
-import javafx.scene.layout.StackPane;
-import javafx.scene.shape.Circle;
-import javafx.stage.Stage;
-public class Main extends Application{                          //laiendamine
-    @Override       //mitte main() meetodiga, vaid start() 
-    public void start(Stage primaryStage) throws Exception {    
-        StackPane stack = new StackPane();
-        Scene scene = new Scene(stack, 150, 150);
-        primaryStage.setScene(scene);
-        Circle ring = new Circle(30);
-        stack.getChildren().add(ring);
-        primaryStage.show();}               }
+public class MinuLoodudKlass extends Application{                          //laiendamine
+    @Override                                       //NB! JavaFX Application: mitte main() meetodiga, vaid start()
+    public void start(Stage primaryStage) throws Exception {               //start()    "primary" pole vajalik
+        primaryStage.setTitle("MinuÄpp");                                 //käsk seotud STAGEga
+        primaryStage.show();                                             //käsk seotud STAGEga
+        //primaryStage.setResizable(boolean);                           //Määra, kas akna suurust saab muuta
+
+        //Stage stage = new Stage();                //loo uus stseen, seda pole vaja???
+        StackPane stack = new StackPane();          //see on vajalik layout stseenile
+        Scene scene = new Scene(stack, 450, 150); //stseeni mõõdud
+        scene.setFill(Paint.valueOf("#ff9900"));                //taustavärv
+        primaryStage.setScene(scene);               //määra milline stseen on aktiivne
+
+        Circle joonistanRingi = new Circle(30);
+        stack.getChildren().add(joonistanRingi);                          //vajalik!!!
+    }}
